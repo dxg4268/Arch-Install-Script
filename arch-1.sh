32621631ec7f83a64dffb653df2cf2fbe402a240
Aditya Sharma <65886209+dxg4268@users.noreply.github.com>
2021-12-11 08:44:04 +0530

    Add files via upload

---
+++ arch-1.sh
@@ -0,0 +1,90 @@
+#Refresh mirrors
+reflector --verbose -c "India" --sort rate > /etc/pacman.d/mirrorlist
+pacman -Syy > /dev/null 2>&1
+echo
+echo
+echo "[+] All Package Databases were updated !"
+echo
+echo
+
+lsblk
+
+read -p "Enter Disk on which Arch is to be installed (eg. sda, vda, sdb): " disk_arch
+
+echo "[!] Warning ! please refer to ArchWiki and do not format your essential data..."
+echo
+sleep 3s
+
+echo "[+] Launching CFDisk Utility for disk management"
+sleep 2s
+cfdisk /dev/$disk_arch
+
+ 
+echo "--------------------------------------------------------"
+echo
+
+lsblk
+
+echo
+echo "--------------------------------------------------------"
+echo
+
+read -p "Enter drive ID from above for Root Install (eg. sda1, vda1) : " root_disk
+echo
+read -p "Enter drive ID from above for SWAP space (eg. sda1, vda1) : " swap_disk
+echo
+echo
+echo " [+] 3 Subvolumes (@, @home, @) on /dev/$root_disk will be created."
+echo " [+] /dev/$swap_disk would be used as SWAP."
+#Disks
+mkfs.btrfs /dev/$root_disk -f > /dev/null 2>&1
+mkswap /dev/$swap_disk > /dev/null 2>&1
+swapon /dev/$swap_disk > /dev/null 2>&1
+mount /dev/$root_disk /mnt
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@var
+umount /mnt
+mount -o noatime,compress=zstd,discard=async,subvol=@ /dev/$root_disk /mnt
+mkdir /mnt/home
+mkdir /mnt/var
+mount -o noatime,compress=zstd,discard=async,subvol=@home /dev/$root_disk /mnt/home
+mount -o noatime,compress=zstd,discard=async,subvol=@var /dev/$root_disk /mnt/var
+
+echo 
+echo "--------------------------------------------------------"
+echo
+
+lsblk
+
+echo
+echo "--------------------------------------------------------"
+echo
+
+sleep 10s
+Base_Install=(base linux-zen linux-firmware sudo nano vi vim intel-ucode btrfs-progs NetworkManager)
+
+echo "[!] This script will install some most important packages so that this computer can be used after FIRST "
+echo "    reboot {base linux-zen linux-firmware sudo nano vi vim Network Manager}"
+echo 
+#echo "[*] If you want to install additional packages, please specify..."
+
+#echo
+#echo "[*] Example, you want to install HTOP with some other packages, Enter a package name and hit enter,"
+#echo "    again type the package and hit enter. "
+
+#echo
+#echo "[*] Enter package name one by one as specified above, Maximum Packages = 10"
+#Install Base System
+echo
+echo
+echo "[+] Installing Packages to the new root, this might take some time according the Internet speed."
+pacstrap /mnt ${Base_Install[*]} > /dev/null 2>&1
+echo
+echo
+
+
+#Generate fstab
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+
---
+++ arch-2.sh
@@ -0,0 +1,42 @@
+#Time
+pacman -S ntp --noconfirm > /dev/null 2>&1
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+timedatectl set-ntp true
+hwclock --systohc
+
+#Locale
+echo 'en_IN UTF-8' >> /etc/locale.gen
+locale-gen
+echo "LANG=en_IN.UTF-8" > /etc/locale.conf
+
+#Hosts
+echo arch > /etc/hostname
+echo "127.0.0.1		localhost" >> /etc/hosts
+echo "::1		      localhost" >> /etc/hosts
+echo "127.0.1.1		arch.localdomain	arch" >> /etc/hosts
+
+#Bootloader
+pacman -S grub grub-btrfs os-prober ntfs-3g --noconfirm > /dev/null 2>&1
+os-prober
+grub-install /dev/sda
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#Extra packages
+pacman -S vlc xorg base-devel git go openbox picom sddm firefox obconf rofi unzip lxrandr lxinput ttf-liberation ttf-dejavu terminator --noconfirm
+
+#Chaotic AUR and imp AUR Packages
+#pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+#pacman-key --lsign-key 3056513887B78AEB
+#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+#echo "[chaotic-aur]" >> /etc/pacman.conf
+#echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+#pacman -Sy pamac-aur archlinux-appstream-data libpamac-aur yay --noconfirm
+
+#enable service
+systemctl enable ufw
+systemctl enable NetworkManager
+systemctl enable sddm
+
