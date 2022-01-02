ccb6f0c32521847e734c3672d9fc64c78c9e17fa
Aditya Sharma <65886209+dxg4268@users.noreply.github.com>
2022-01-02 09:57:55 +0530

    Update arch-2.sh

--- arch-2.sh
+++ arch-2.sh
@@ -93,7 +93,7 @@ echo
 echo
 echo "---------------------------------------------------------------"
 echo
-pacman -S xorg xorg-xinit xfce4 xfce4-goodies papirus-icon-theme base-devel openbox bspwm sxhkd rofi dunst nitrogen zsh-syntax-highlighting zsh-autosuggestions starship zsh-history-substring-search pkgfile fzf pipewire pipewire-pulse pipewire-alsa git unzip ttf-liberation ttf-dejavu ttf-indic-otf zsh nemo sddm ufw ttf-roboto ttf-jetbrains-mono terminator --noconfirm --needed
+pacman -S xorg xorg-xinit xfce4 xfce4-goodies papirus-icon-theme arc-gtk-theme archlinux-keyring base-devel openbox bspwm sxhkd rofi dunst nitrogen zsh-syntax-highlighting zsh-autosuggestions starship zsh-history-substring-search pkgfile fzf pipewire pipewire-pulse pipewire-alsa git unzip ttf-liberation ttf-dejavu ttf-indic-otf zsh nemo sddm ufw ttf-roboto ttf-jetbrains-mono terminator --noconfirm --needed
 echo
 echo "[*] Packages Installed Successfully..."
 echo
